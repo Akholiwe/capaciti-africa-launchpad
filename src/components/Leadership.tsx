ae3f2b0020606d1d760a1d143ae2cb32c836a6e6
AkonMabni <66824643+Akholiwe@users.noreply.github.com>
2025-08-27 10:55:31 +0200

    Update Leadership.tsx

--- src/components/Leadership.tsx
+++ src/components/Leadership.tsx
@@ -57,7 +57,7 @@ const Leadership = () => {
                     <img 
                       src={leader.image} 
                       alt={leader.name}
-                      className="w-full h-full object-cover object-center"
+                      className="w-full h-full object-cover object-top"
                     />
                   </div>
                 ) : (
